adf491b9cbeec665af6b3c70a261af00238101e1
Daniel W. Hieber <dwhieb@users.noreply.github.com>
2016-08-18 21:27:44 -0500

    Blog (#34)
    * add content to admin landing page (admin/index.js)
    
    * add blog subscribe link on Home
    
    * add blog subscription button to home.hbs
    
    * add styling and functionality for subscribe to blog button
    
    * add recent posts to Home page
    
    closes #33
    
    * add simplified display of recent posts on error
    
    * fix styling issue with error fallback

---
+++ public/js/posts.js
@@ -0,0 +1,63 @@
+/* eslint-env browser */
+/* global ghost */
+
+(function displayPosts() {
+
+  const ghostOptions = {
+    limit: 5,
+    fields: 'title,slug,image,updated_at,html',
+  };
+
+  const renderPosts = posts => {
+
+    const wrapper = document.querySelector('#posts ol');
+
+    if (posts.length === 0) {
+
+      wrapper.innerHTML = `
+        <p><a class=error-fallback href=http://blog.danielhieber.com/>Check out my blog for recent posts!</a></p>
+      `;
+
+    } else {
+
+      posts.forEach(post => {
+
+        const postLink = `http://blog.danielhieber.com/${post.slug}`;
+        const d = new Date(post.updated_at);
+        const dateString = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+        const img = post.image ? `<img src=${post.image}>` : '';
+
+        // get a preview of the post's text content
+        const p = document.createElement('p');
+        p.innerHTML = post.html;
+        const preview = p.textContent.substring(0, 240);
+
+        const html = `
+          <li>
+            <a href=${postLink}>
+              ${img}
+              <div>
+                <h2>${post.title}</h2>
+                <time datetime=${post.updated_at}>${dateString}</time>
+                <p>${preview} ... <span>(read more)<span></p>
+              </div>
+            </a>
+          </li>
+        `;
+
+        wrapper.insertAdjacentHTML('beforeend', html);
+
+      });
+    }
+  };
+
+  ghost.init({
+    clientId: 'ghost-frontend',
+    clientSecret: 'c7441a524886',
+  });
+
+  fetch(ghost.url.api('posts', ghostOptions))
+  .then(res => res.json().then(data => renderPosts(data.posts)))
+  .catch(() => renderPosts([]));
+
+}());
