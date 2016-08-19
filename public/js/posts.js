291c254eece27246e27130a06b455f7feb5bb0a3
Daniel W. Hieber <dwhieb@gmail.com>
2016-08-18 21:21:03 -0500

    add simplified display of recent posts on error

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,43 +1,55 @@
 /* eslint-env browser */
 /* global ghost */
 
-(function posts() {
+(function displayPosts() {
 
   const ghostOptions = {
     limit: 5,
     fields: 'title,slug,image,updated_at,html',
   };
 
-  const renderPosts = posts => posts.forEach(post => {
+  const renderPosts = posts => {
 
-    const postLink = `http://blog.danielhieber.com/${post.slug}`;
-    const d = new Date(post.updated_at);
-    const dateString = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-    const img = post.image ? `<img src=${post.image}>` : '';
+    const wrapper = document.querySelector('#posts ol');
 
-    // get a preview of the post's text content
-    const p = document.createElement('p');
-    p.innerHTML = post.html;
-    const preview = p.textContent.substring(0, 240);
+    if (posts.length === 0) {
 
-    const html = `
-      <li>
-        <a href=${postLink}>
-          ${img}
-          <div>
-            <h2>${post.title}</h2>
-            <time datetime=${post.updated_at}>${dateString}</time>
-            <p>${preview} ... <span>(read more)<span></p>
-          </div>
-        </a>
-      </li>
-    `;
+      wrapper.innerHTML = `
+        <p><a href=http://blog.danielhieber.com/>Check out my blog for recent posts!</a></p>.
+      `;
 
-    wrapper.insertAdjacentHTML('beforeend', html);
+    } else {
 
-  });
+      posts.forEach(post => {
 
-  const wrapper = document.querySelector('#posts ol');
+        const postLink = `http://blog.danielhieber.com/${post.slug}`;
+        const d = new Date(post.updated_at);
+        const dateString = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+        const img = post.image ? `<img src=${post.image}>` : '';
+
+        // get a preview of the post's text content
+        const p = document.createElement('p');
+        p.innerHTML = post.html;
+        const preview = p.textContent.substring(0, 240);
+
+        const html = `
+          <li>
+            <a href=${postLink}>
+              ${img}
+              <div>
+                <h2>${post.title}</h2>
+                <time datetime=${post.updated_at}>${dateString}</time>
+                <p>${preview} ... <span>(read more)<span></p>
+              </div>
+            </a>
+          </li>
+        `;
+
+        wrapper.insertAdjacentHTML('beforeend', html);
+
+      });
+    }
+  };
 
   ghost.init({
     clientId: 'ghost-frontend',
