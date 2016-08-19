d7a4a2ee77638a0c8b5ca8c202d95415a927eb0d
Daniel W. Hieber <dwhieb@users.noreply.github.com>
2016-08-19 14:39:29 -0500

    Gallery (#41)
    * finish gallery transitions
    
    closes #39
    
    * attempt 1
    
    fixing image transition timing on dev

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,9 +1,54 @@
+/* eslint-env browser */
+/* global socket */
 (function runGallery() {
 
+  let currentImage;
+  const fallbackImage = 'cypress_trees.jpg';
   const gallery = document.getElementById('gallery');
+  const images = [];
+  const opacityTransition = 500;
+  const transitionInterval = 10000;
 
-  console.log(gallery);
+  const changeImage = img => {
+    gallery.src = `/img/gallery/${img}`;
+  };
 
-  socket.on('gallery', images => {});
+  const getRandomImage = () => {
+
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    if (randomImage === currentImage) {
+      return getRandomImage();
+    }
+
+    currentImage = randomImage;
+
+    return randomImage;
+
+  };
+
+  const toggleOpacity = () => {
+    gallery.style.opacity = gallery.style.opacity === '0' ? '0.2' : '0';
+  };
+
+  const advance = () => {
+
+    const img = getRandomImage();
+
+    toggleOpacity();
+    setTimeout(changeImage, opacityTransition/2, img);
+    setTimeout(toggleOpacity, opacityTransition);
+
+  };
+
+  if (socket) {
+    socket.on('gallery', imageList => {
+      images.push(...imageList);
+      changeImage(getRandomImage());
+      setInterval(advance, transitionInterval);
+    });
+  } else {
+    changeImage(fallbackImage);
+  }
 
 }());
