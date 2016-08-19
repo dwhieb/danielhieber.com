00412d8c0376170e9cebe09267f937250e7670d3
Daniel W. Hieber <dwhieb@gmail.com>
2016-08-19 14:37:50 -0500

    attempt 1
    fixing image transition timing on dev

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -9,6 +9,10 @@
   const opacityTransition = 500;
   const transitionInterval = 10000;
 
+  const changeImage = img => {
+    gallery.src = `/img/gallery/${img}`;
+  };
+
   const getRandomImage = () => {
 
     const randomImage = images[Math.floor(Math.random() * images.length)];
@@ -27,27 +31,24 @@
     gallery.style.opacity = gallery.style.opacity === '0' ? '0.2' : '0';
   };
 
-  const changeImage = () => {
+  const advance = () => {
 
     const img = getRandomImage();
 
     toggleOpacity();
-
-    setTimeout(() => {
-      gallery.src = `/img/gallery/${img}`;
-      toggleOpacity();
-    }, opacityTransition);
+    setTimeout(changeImage, opacityTransition/2, img);
+    setTimeout(toggleOpacity, opacityTransition);
 
   };
 
   if (socket) {
     socket.on('gallery', imageList => {
       images.push(...imageList);
-      gallery.src = `/img/gallery/${getRandomImage()}`;
-      setInterval(changeImage, transitionInterval);
+      changeImage(getRandomImage());
+      setInterval(advance, transitionInterval);
     });
   } else {
-    gallery.src = `/img/gallery/${fallbackImage}`;
+    changeImage(fallbackImage);
   }
 
 }());
