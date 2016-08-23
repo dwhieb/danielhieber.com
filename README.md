ddb8df999cc33276e4894fa88f03533ef64d08b5
Daniel W. Hieber <dwhieb@gmail.com>
2016-08-23 18:56:15 -0500

    add Passport and sessions

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # danielhieber.com
 [![Build Status](https://travis-ci.org/dwhieb/danielhieber.com.svg?branch=master)](https://travis-ci.org/dwhieb/danielhieber.com)
+[![Dependencies Status](https://david-dm.org/dwhieb/danielhieber.com.svg)](https://github.com/dwhieb/danielhieber.com#readme)
 
 The academic homepage of Daniel W. Hieber, graduate student in linguistics at the University of California, Santa Barbara.
 
--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const config = require('./lib/config');
+
+const auth = require('./lib/auth');
 const appInsights = require('applicationinsights');
 const express = require('express');
 const Handlebars = require('express-handlebars');
 const helmet = require('helmet');
 const http = require('http');
-const IO = require('socket.io');
+const socketIO = require('socket.io');
 const meta = require('./package.json');
+const passport = require('passport');
 const path = require('path');
 const middleware = require('./lib/middleware');
 const router = require('./lib/router');
+const session = require('express-session');
 const socket = require('./lib/socket');
+const WindowsLiveStrategy = require('passport-windowslive');
 
 // initialize Express, Handlebars, & Passport
 const app = express();
 const handlebars = Handlebars.create(config.hbsOptions);
+
+passport.use(new WindowsLiveStrategy(
+  config.windowsLiveStrategyConfig,
+  auth.verifyWindowsLiveStrategy
+));
+passport.serializeUser(auth.serializeUser);
+passport.deserializeUser(auth.deserializeUser);
 
 // Azure application insights
 if (config.env === 'production') appInsights.setup().start();
@@ -28,6 +40,9 @@ app.locals.meta = meta; // makes package.json data available for templating
 // middleware
 app.use(helmet()); // basic security features
 app.use(express.static(path.join(__dirname, '/public'))); // routing for static files
+app.use(session(config.sessionOptions)); // use sessions
+app.use(passport.initialize()); // initialize Passport
+app.use(passport.session(config.sessionOptions)); // persist user in session with Passport
 app.use(middleware); // custom middleware (logs URL, injects variables, etc.)
 
 // URL routing
@@ -47,7 +62,7 @@ server.listen(config.port, () => {
 });
 
 // create web socket
-const io = IO(server, config.socketOptions); // eslint-disable-line new-cap
+const io = socketIO(server, config.socketOptions);
 
 // socket routing
 socket(io);
