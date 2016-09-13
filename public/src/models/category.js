0c0b0cd3fc19c222ff8e9ac9d31818142b68dbfe
Daniel W. Hieber <dwhieb@gmail.com>
2016-09-13 17:56:00 -0500

    comment category.js

--- public/src/models/category.js
+++ public/src/models/category.js
@@ -1,16 +1,31 @@
-/* global Emitter, Model */
+/* global Model */
 
+// the default Category data to use if none is provided
 const defaults = {
   id:          '',
   name:        '',
   description: '',
 };
 
+/**
+ * A class representing a Category
+ * @type {Object} Category
+ */
 const Category = class Category extends Model {
+  /**
+   * Create a new instance of a Category
+   * @type {Object} Category
+   * @param {Object} [data]     The JSON data to create the category from
+   */
   constructor(data = defaults) {
     super(data);
   }
 
+  /**
+   * Delete this category from the database
+   * @method
+   * @return {Promise} Resolves to a JSON object when the category is deleted
+   */
   delete() {
     return new Promise((resolve, reject) => {
       socket.emit('deleteCategory', this, (err, res) => {
@@ -20,6 +35,11 @@ const Category = class Category extends Model {
     });
   }
 
+  /**
+   * Save (upsert) this category to the database
+   * @method
+   * @return {Promise} Resolves to a JSON object when the category is saved
+   */
   save() {
     return new Promise((resolve, reject) => {
       socket.emit('updateCategory', this, (err, res) => {
