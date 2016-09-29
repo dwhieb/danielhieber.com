eba07bdaec5bd11baefb76fd7f93d996fb3f61eb
Daniel W. Hieber <dwhieb@gmail.com>
2016-09-28 21:56:32 -0500

    comment Award model

--- models/award.js
+++ models/award.js
@@ -7,23 +7,29 @@ const Document = require('./document');
 const Award = class Award extends Document {
   constructor(data = {}) {
 
+    // create an empty object to copy data over to
     const award = {};
 
+    // required attributes
     const required = [
       'title',
       'year',
     ];
 
+    // copy whitelisted attributes
     Award.whitelist.forEach(attr => {
       if (attr in data) award[attr] = data[attr];
     });
 
+    // check for required attributes
     required.forEach(attr => {
       if (!(attr in award)) throw new Error(`The award data must include a "${attr}" attribute.`);
     });
 
+    // set document type to "award"
     award.type = 'award';
 
+    // the list of preset properties to use
     const props = [
       'categories',
       'description',
@@ -32,10 +38,12 @@ const Award = class Award extends Document {
       'year',
     ];
 
+    // instantiate the new document
     super(award, props);
 
   }
 
+  // the allowable properties for this model
   static get whitelist() {
     return Document.whitelist.concat([
       'categories',
