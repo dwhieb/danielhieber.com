d6d3bc1a7a0b05ba9457f4f739acaa1679031c5f
Daniel W. Hieber <dwhieb@gmail.com>
2016-10-02 23:25:54 -0500

    preparations

--- public/js/core/emitter.js
+++ public/js/core/emitter.js
@@ -1,134 +1,170 @@
-/** A class representing an Event Emitter */
-const Emitter = class Emitter {
-  /**
-   * Create a new Emitter instance
-   * @class
-   * @prop {Object} listeners         An object containing all the listeners for each event type
-   */
-  constructor() {
-    Object.defineProperty(this, 'listeners', {
-      value: {}
-    });
-  }
+const Emitter = function () {
 
-  /**
-   * Calls the callback functions registered for the given event name
-   * @method
-   * @param {String} eventName    The event name to fire
-   * @param {Any}    [args]       The remaining arguments to pass to the event callback
-   */
-  emit(eventName, ...args) {
-    if (eventName in this.listeners) {
-      this.listeners[eventName].forEach(cb => cb(...args));
+  const listeners = {};
+
+  const assign = (target, src) => Object.getOwnPropertyNames(src).forEach(prop => {
+    if (prop !== 'constructor') {
+      const propertyDescriptor = Object.getOwnPropertyDescriptor(src, prop);
+      Object.defineProperty(target, prop, propertyDescriptor);
     }
-  }
+  });
 
-  /**
-   * Removes listeners from the object. If no arguments are provided, all listeners are removed. If only an eventName is provided, all listeners are removed for that event name. If only a listener is provided, that listener is removed from all events. If both an event name and listener are provided, only that particular listener for that particular event is removed.
-   * @method
-   * @param {String} [eventName]       The event to remove listeners for.
-   * @param {Function} [listener]      The listener to remove.
-   */
-  off(eventName, listener) {
-
-    if (eventName && typeof eventName !== 'string' && typeof eventName !== 'function') {
-      throw new Error(`The .off() method should be passed an event name (String), listener (Function), both, or neither.`);
+  const Emitter = class Emitter {
+    constructor() {
+      Object.defineProperties(Emitter.prototype, {});
     }
 
-    const event = typeof eventName === 'string' ? eventName : undefined;
-    const cb = typeof eventName === 'function' ? eventName : listener;
-
-    if (event && !(event in this.listeners)) {
-      throw new Error(`No listeners for "${ eventName }" exist.`);
-    }
-
-    const removeListener = (evName, listenerFunc) => {
-
-      const i = this.listeners[evName].findIndex(func => Object.is(func, listenerFunc));
-
-      // this requires explicit comparison b/c the index may sometimes be zero
-      if (i >= 0) {
-        this.listeners[evName].splice(i, 1);
-        if (this.listeners[evName].length === 0) delete this.listeners[evName];
-        return true;
-      }
-
-      return false;
-    };
-
-    if (event && cb) {
-
-      removeListener(event, cb);
-    } else if (cb) {
-
-      for (const evName in this.listeners) {
-        removeListener(evName, cb);
-      }
-    } else if (event) {
-
-      delete this.listeners[event];
-    } else {
-
-      for (const prop in this.listeners) {
-        delete this.listeners[prop];
+    /**
+     * Calls the callback functions registered for the given event name
+     * @method
+     * @param {String} eventName    The event name to fire
+     * @param {Any}    [args]       The remaining arguments to pass to the event callback
+     */
+    emit(eventName, ...args) {
+      if (eventName in listeners) {
+        listeners[eventName].forEach(cb => cb(...args));
       }
     }
-  }
 
-  /**
-   * Attaches a listener for the given event name
-   * @method
-   * @param {String} eventName      The name of the event to attach the listener to
-   * @param {Function} cb           The callback function (the listener) to fire each time the event is triggered
-   */
-  on(eventName, cb) {
-    if (typeof eventName !== 'string') throw new Error('Event name must be a string.');
-    if (typeof cb !== 'function') throw new Error('Callback must be a function.');
+    /**
+     * Retrieves a copy of the listeners object.
+     * @method
+     * @param {String} [eventName]      Pass the name of the event if you only want listeners returned for that event
+     * @return {Object} listeners       Returns a shallow copy of the listeners object
+     */
+    getListeners(eventName) {
+      if (eventName && eventName in listeners) {
 
-    this.listeners[eventName] = this.listeners[eventName] || [];
-    this.listeners[eventName].push(cb);
-  }
+        return Array.from(listeners[eventName]);
+      } else if (eventName) {
 
-  /**
-   * Attach a listener to an event that will only fire the first time the event occurs
-   * @method
-   * @param {String} eventName      The name of the event to attach the listener to
-   * @param {Function} cb           The callback function (the listener) to fire when the event is triggered. This callback will only fire once.
-   */
-  once(eventName, cb) {
-    if (typeof cb !== 'function') throw new Error('Callback must be a function.');
+        throw new Error(`Event "${ eventName }" not found in listeners.`);
+      } else {
 
-    const proxyHandler = (...args) => {
-      this.off(eventName, proxyHandler);
-      cb(...args);
-    };
+        const listenersObj = {};
 
-    this.on(eventName, proxyHandler);
-  }
+        for (const evName in listeners) {
+          if (listeners.hasOwnProperty(evName)) {
+            listenersObj[evName] = Array.from(listeners[evName]);
+          }
+        }
 
-  /**
-   * Extends
-   * @static
-   * @function
-   * @param {Object} obj        The object to extend the Event Emitter methods to
-   * @return {Object} obj       Returns the original object, with the Event Emitter methods attached
-   */
-  static extend(obj = {}) {
-
-    const target = obj.prototype || obj;
-    const source = this.prototype;
-
-    const assign = (tgt, src) => Object.getOwnPropertyNames(src).forEach(prop => {
-      if (prop !== 'constructor') {
-        const propertyDescriptor = Object.getOwnPropertyDescriptor(src, prop);
-        Object.defineProperty(tgt, prop, propertyDescriptor);
+        return listenersObj;
       }
-    });
+    }
 
-    assign(target, source);
-    assign(target, new Emitter());
+    /**
+     * Removes listeners from the object. If no arguments are provided, all listeners are removed. If only an eventName is provided, all listeners are removed for that event name. If only a listener is provided, that listener is removed from all events. If both an event name and listener are provided, only that particular listener for that particular event is removed.
+     * @method
+     * @param {String} [eventName]       The event to remove listeners for.
+     * @param {Function} [listener]      The listener to remove.
+     */
+    off(...args) {
 
-    return target;
-  }
+      if (args.length && !(typeof args[0] === 'string' || typeof args[0] === 'function')) {
+        throw new Error(`The .off() method should be passed an event name (String), listener (Function), both, or neither.`);
+      }
 
-};
+      const eventName = typeof args[0] === 'string' ? args[0] : null;
+      const cb = typeof args[0] === 'function' ? args[0] : args[1];
+
+      if (eventName && !(eventName in listeners)) {
+        throw new Error(`No listeners for "${ eventName }" exist.`);
+      }
+
+      // removes an individual listener from an event
+      const removeListener = (evName, callback) => {
+
+        const i = listeners[evName].indexOf(callback);
+
+        if (i >= 0) {
+          listeners[evName].delete(callback);
+          if (!listeners[evName].size) delete listeners[evName];
+          return true;
+        }
+
+        return false;
+      };
+
+      // remove a single listener from a specific event
+      if (eventName && cb) {
+
+        removeListener(eventName, cb);
+
+        // remove a specific listener from all events
+      } else if (cb) {
+
+        for (const ev in listeners) {
+          if (listeners.hasOwnProperty(ev)) {
+            removeListener(eventName, cb);
+          }
+        }
+
+        // remove all the listeners for an event
+      } else if (eventName) {
+
+        delete listeners[eventName];
+      } else {
+        for (const ev in listeners) {
+          if (listeners.hasOwnProperty(ev)) {
+            delete listeners[ev];
+          }
+        }
+      }
+    }
+
+    /**
+     * Attaches a listener for the given event name
+     * @method
+     * @param {String} eventName      The name of the event to attach the listener to
+     * @param {Function} cb           The callback function (the listener) to fire each time the event is triggered
+     */
+    on(eventName, cb) {
+
+      if (typeof eventName !== 'string') throw new Error('Event name must be a string.');
+      if (typeof cb !== 'function') throw new Error('Callback must be a function.');
+
+      if (!(eventName in listeners)) {
+        listeners[eventName] = new Set();
+      }
+
+      listeners[eventName].add(cb);
+    }
+
+    /**
+     * Attach a listener to an event that will only fire the first time the event occurs
+     * @method
+     * @param {String} eventName      The name of the event to attach the listener to
+     * @param {Function} cb           The callback function (the listener) to fire when the event is triggered. This callback will only fire once.
+     */
+    once(eventName, cb) {
+
+      // cb must be a function
+      if (typeof cb !== 'function') throw new Error('Callback must be a function.');
+
+      // removes the event listener, then runs the original callback
+      const proxyHandler = (...args) => {
+        this.off(eventName, proxyHandler);
+        cb(...args);
+      };
+
+      this.on(eventName, proxyHandler);
+    }
+
+    /**
+     * Extends the Event Emitter to another object, making that object an Emitter mixin
+     * @static
+     * @function
+     * @param {Object} obj        The object to extend the Event Emitter methods to
+     * @return {Object} obj       Returns the original object, with the Event Emitter methods attached
+     */
+    static extend(obj = {}) {
+      assign(obj, this.prototype);
+      assign(obj, new Emitter());
+      return obj;
+    }
+
+  };
+
+  return Emitter;
+}();
