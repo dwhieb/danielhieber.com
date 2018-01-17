c2a972c4e15c67e41575b1893cf5664e7ecd8504
Daniel W. Hieber <dwhieb@gmail.com>
2018-01-17 13:34:50 -0600

    add Handlebars

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const config  = require('./lib/config');
 
 // modules
 const express     = require('express');
+const hbs         = require('./lib/handlebars');
 const helmet      = require('./lib/helmet');
 const meta        = require('./package.json');
 const route       = require('./lib/router');
@@ -12,9 +13,11 @@ const startServer = require('./lib/server');
 const app = express();
 
 // app settings
-app.enable(`trust proxy`);    // trust the Azure proxy server
-app.locals.meta = meta;       // makes package.json data available to app and middleware
-app.set(`port`, config.port); // set port
+app.enable(`trust proxy`);           // trust the Azure proxy server
+app.engine(hbs.extname, hbs.engine); // declare Handlebars engine
+app.set(`port`, config.port);        // set port
+app.set(`view engine`, hbs.extname); // use Handlebars for templating
+app.locals.meta = meta;              // makes package.json data available to app and middleware
 
 // middleware
 app.use(helmet);
