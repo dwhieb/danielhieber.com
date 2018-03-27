10ba397ecb4095a1a5ee95a861520c141d41dec1
Daniel W. Hieber <dwhieb@gmail.com>
2018-03-27 15:02:34 -0500

    add and delete achievements
    closes #373
    closes #372

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,11 +1,54 @@
 "use strict";
 
+// Client-side JavaScript for the admin Editor page
+
 /* eslint-env browser */
 
+/* eslint-disable
+  no-alert,
+*/
+
+// Elements
+var achievementTemplate = document.getElementById("achievement-template");
+var achievementsList = document.querySelector(".achievements ul");
+var addAchievementButton = document.getElementById("addAchievementButton");
 var dropdown = document.getElementById("dropdown");
+
+// Handlers
+var addAchievement = function addAchievement() {
+  var li = achievementTemplate.content.querySelector("li").cloneNode(true);
+  achievementsList.appendChild(li);
+};
+
+var deleteAchievement = function deleteAchievement(ev) {
+
+  var isButton = function isButton(node) {
+    if (node.classList.contains("trash")) return true;
+    if (node === achievementsList) return false;
+    if (node.parentNode) return isButton(node.parentNode);
+    return false;
+  };
+
+  var getListItem = function getListItem(node) {
+    if (node.tagName === "LI") return node;
+    return getListItem(node.parentNode);
+  };
+
+  if (!isButton(ev.target)) return;
+
+  var li = getListItem(ev.target);
+
+  var confirmed = confirm("Are you sure you want to delete this achievement?");
+
+  if (confirmed) li.remove();
+};
 
 var updateType = function updateType(ev) {
   window.location = "/admin/" + ev.target.value;
 };
 
-dropdown.onchange = updateType; //# sourceMappingURL=/js/editor.js.map
+// Attach handlers
+dropdown.onchange = updateType;
+
+if (achievementsList) achievementsList.addEventListener("click", deleteAchievement);
+if (addAchievementButton) addAchievementButton.onclick = addAchievement; //# sourceMappingURL=/js/editor.js.map
--- views/editor/editor.js
+++ views/editor/editor.js
@@ -1,9 +1,53 @@
+// Client-side JavaScript for the admin Editor page
+
 /* eslint-env browser */
 
-const dropdown = document.getElementById(`dropdown`);
+/* eslint-disable
+  no-alert,
+*/
+
+// Elements
+const achievementTemplate  = document.getElementById(`achievement-template`);
+const achievementsList     = document.querySelector(`.achievements ul`);
+const addAchievementButton = document.getElementById(`addAchievementButton`);
+const dropdown             = document.getElementById(`dropdown`);
+
+// Handlers
+const addAchievement = () => {
+  const li = achievementTemplate.content.querySelector(`li`).cloneNode(true);
+  achievementsList.appendChild(li);
+};
+
+const deleteAchievement = ev => {
+
+  const isButton = node => {
+    if (node.classList.contains(`trash`)) return true;
+    if (node === achievementsList) return false;
+    if (node.parentNode) return isButton(node.parentNode);
+    return false;
+  };
+
+  const getListItem = node => {
+    if (node.tagName === `LI`) return node;
+    return getListItem(node.parentNode);
+  };
+
+  if (!isButton(ev.target)) return;
+
+  const li = getListItem(ev.target);
+
+  const confirmed = confirm(`Are you sure you want to delete this achievement?`);
+
+  if (confirmed) li.remove();
+
+};
 
 const updateType = ev => {
   window.location = `/admin/${ev.target.value}`;
 };
 
+// Attach handlers
 dropdown.onchange = updateType;
+
+if (achievementsList) achievementsList.addEventListener(`click`, deleteAchievement);
+if (addAchievementButton) addAchievementButton.onclick = addAchievement;
