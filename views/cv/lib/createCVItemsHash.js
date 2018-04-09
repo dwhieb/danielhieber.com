7191002be6248e32c61cc4e5d8377f35c6b604a0
Daniel W. Hieber <dwhieb@gmail.com>
2018-04-09 12:53:53 -0500

    HTML for CV publications section

--- views/cv/lib/createCVItemsHash.js
+++ views/cv/lib/createCVItemsHash.js
@@ -1,3 +1,7 @@
+/* eslint-disable
+  no-param-reassign,
+*/
+
 const { compare }           = require('../../../lib/utilities');
 const { CVTypes, pubTypes } = require('../../../lib/constants');
 
@@ -6,14 +10,9 @@ module.exports = docs => {
   // Create CV items hash
   const items = Object.keys(CVTypes)
   .reduce((hash, collType) => {
-
     if (collType === `categories` || collType === `test`) return hash;
-
-    // eslint-disable-next-line no-param-reassign
     hash[collType] = docs.filter(doc => doc.type === CVTypes[collType].type && !doc.hidden);
-
     return hash;
-
   }, {});
 
   // Sort CV items as appropriate
@@ -27,7 +26,8 @@ module.exports = docs => {
   items.media.sort((a, b) => compare(b.date, a.date));                       // date descending
   items.memberships.sort((a, b) => compare(a.organization, b.organization)); // organization ascending
   items.proficiencies.sort((a, b) => compare(a.title, b.title));             // title ascending
-  items.publications.sort((a, b) => compare(b.date, a.date));                // date descending
+  items.publications.sort((a, b) => compare(b.date, a.date)                  // date descending, title ascending
+    || compare(a.title, b.title));
   items.references.sort((a, b) => compare(a.priority, b.priority));          // priority ascending
   items.service.sort((a, b) => compare(b.startYear, a.startYear)             // startYear, endYear descending
     || compare(b.endYear, a.endYear));
@@ -38,11 +38,18 @@ module.exports = docs => {
   items.publications = Object.keys(pubTypes)
   .reduce((hash, pubType) => {
 
-    // eslint-disable-next-line no-param-reassign
+    // Create an array for each pubType
     hash[pubType] = items.publications.filter(doc => doc.publicationType === pubType);
 
-    return hash;
+    // Add title for rendering
+    hash[pubType].title = pubTypes[pubType];
 
+    // Add "year" attribute for rendering
+    hash[pubType].forEach(doc => {
+      doc.year = new Date(doc.date).getFullYear();
+    });
+
+    return hash;
   }, {});
 
   return items;
