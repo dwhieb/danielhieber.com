39252743ebb7c8ea31cb0807b8d42f5e8a4df550
Daniel W. Hieber <dwhieb@gmail.com>
2018-04-12 13:24:39 -0500

    bug fixes 5

--- views/cv/handlers/getPDF.js
+++ views/cv/handlers/getPDF.js
@@ -4,13 +4,19 @@ const { promisify } = require('util');
 
 module.exports = async (req, res, next) => {
 
+  console.log('Script got to handler');
+
   await createPDF();
+
+  console.log('Script created PDF');
 
   const sendFile = await promisify(res.sendFile).bind(res);
 
   try {
     await sendFile(path.join(__dirname, `../cv.pdf`));
   } catch (e) {
+    console.log('Script got an error');
+    console.error(e);
     return next(e);
   }
 
