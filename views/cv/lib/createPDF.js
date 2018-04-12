8f39b63e934b20b9d7678baf8070360a1e889c61
Daniel W. Hieber <dwhieb@gmail.com>
2018-04-12 14:46:36 -0500

    bug fixes 10

--- views/cv/lib/createPDF.js
+++ views/cv/lib/createPDF.js
@@ -11,12 +11,12 @@ module.exports = async () => {
     const { stdout, stderr } = await prince()
     .binary(princePath)
     .option(`baseurl`, `${baseURL}/cv`)
-    .option(`no-warn-css`, true, true)
-    .option(`media`, `print`)
-    .option(`page-size`, `letter`, true)
-    .option(`page-margin`, `1in`, true)
-    .option(`pdf-title`, `Daniel W. Hieber - Curriculum Vitae`)
-    .option(`pdf-author`, `Daniel W. Hieber`)
+    // .option(`no-warn-css`, true, true)
+    // .option(`media`, `print`)
+    // .option(`page-size`, `letter`, true)
+    // .option(`page-margin`, `1in`, true)
+    // .option(`pdf-title`, `Daniel W. Hieber - Curriculum Vitae`)
+    // .option(`pdf-author`, `Daniel W. Hieber`)
     .inputs(`${baseURL}/cv`)
     .output(path.join(__dirname, `../cv.pdf`))
     .execute();
@@ -27,7 +27,7 @@ module.exports = async () => {
 
   } catch (e) {
 
-    console.error(e);
+    console.error(e.error);
     throw e;
 
   }
