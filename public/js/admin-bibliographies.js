f9e57e91f0ab3ed16ece44f84daf110ed42d94d3
Daniel W. Hieber <dwhieb@gmail.com>
2018-05-08 11:02:22 -0500

    NEW: confirm deletion of Bibliography

--- public/js/admin-bibliographies.js
+++ public/js/admin-bibliographies.js
@@ -1,1 +1,20 @@
-"use strict"; //# sourceMappingURL=/js/admin-bibliographies.js.map
+"use strict";
+
+/**
+ * Client-side JavaScript for the Bibliographies Editor
+ */
+
+/* eslint-env browser */
+
+/* eslint-disable
+  no-alert,
+*/
+
+var deleteButton = document.getElementById("deleteButton");
+
+var confirmDeletion = function confirmDeletion(ev) {
+  var confirmed = confirm("Are you sure you want to delete this bibliography? It will still be available in CosmosDB for 30 days.");
+  if (!confirmed) ev.preventDefault();
+};
+
+deleteButton.addEventListener("click", confirmDeletion); //# sourceMappingURL=/js/admin-bibliographies.js.map
