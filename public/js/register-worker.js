97b5541b94d52c57bd3ede81a0eb7157c5c0a2eb
Daniel W. Hieber <dwhieb@gmail.com>
2018-05-13 13:17:43 -0500

    BUILD

--- public/js/register-worker.js
+++ public/js/register-worker.js
@@ -1,9 +1,1 @@
-"use strict";
-
-/**
- * This script registers the service worker that caches items for offline use
- */
-
-if ("serviceWorker" in navigator && location.protocol !== "file:" && navigator.onLine) {
-  navigator.serviceWorker.register("/offline-worker.js", { scope: "/" }).catch(console.error);
-} //# sourceMappingURL=/js/register-worker.js.map
+"use strict";if("serviceWorker"in navigator&&location.protocol!=="file:"&&navigator.onLine){navigator.serviceWorker.register("/offline-worker.js",{scope:"/"}).catch(console.error)} //# sourceMappingURL=/js/register-worker.js.map
