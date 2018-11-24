ec6c2c9b88ed287aa51b5b7c488089a0eb1f200a
dwhieb <dwhieb@gmail.com>
2018-11-23 21:23:36 -0600

    sort items on /research page
    closes #443

--- views/research/handlers/get.js
+++ views/research/handlers/get.js
@@ -2,13 +2,16 @@
  * GET handler for the Research page
  */
 
-const { db } = require('../../../services');
+const { compare } = require(`../../../utilities`);
+const { db }      = require(`../../../services`);
 
 module.exports = async (req, res) => {
 
   const items = await db.getByType(`category`);
 
-  items.forEach(cat => {
+  items
+  .sort((a, b) => compare(a.priority, b.priority))
+  .forEach(cat => {
     // TODO: Remove this when issue #384 is closed
     // eslint-disable-next-line no-param-reassign
     cat.description = cat.markdown;
